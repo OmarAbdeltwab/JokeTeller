2f2ac004bb30931e32b874afbe9c51f89e4deed9
Omar Mohamed <maom3394@gmail.com>
2018-07-02 15:07:50 +0200

    reset ip to work with Emulator not a local device

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -72,7 +72,7 @@ new EndpointsAsyncTask().execute( view.getContext() );
                         // - 10.0.2.2 is localhost's IP address in Android emulator
                         // - turn off compression when running against local devappserver
                         //
-                        .setRootUrl("http://192.168.1.122:8080/_ah/api/")
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                         .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                             @Override
                             public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
