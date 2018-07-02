6aa8426f97b0b461b761f2e7ed470cbe0cc6fdf7
Omar Mohamed <maom3394@gmail.com>
2018-07-02 14:37:17 +0200

    Added flavors

---
+++ JokeTellerLib/src/main/java/com/example/android/joketellerlib/JokeTeller.java
@@ -0,0 +1,18 @@
+package com.example.android.joketellerlib;
+
+public class JokeTeller {
+
+    private static final String[] Jokes= new String[]{"What did the traffic light say to the car? \n   Don’t look! I’m about to change"
+            , "Why was the little strawberry crying?\n His mom was in a jam.",
+
+            "Why are frogs are so happy?\n They eat whatever bugs them.",
+            "How do you befriend a squirrel?\n Just act like a nut.",
+            "Have you heard about the corduroy pillow?\n No? Really? It’s making headlines!",
+            "Why did the jaguar eat the tightrope walker?\n    It was craving a well-balanced meal"};
+
+
+    static public String GetTheJoke(){
+
+        return Jokes[(int) Math.floor(Math.random() * 6)];}
+
+}
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,106 @@
+package com.udacity.gradle.builditbigger;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+import com.example.android.joketellerlib.JokeTeller;
+import com.example.android.jokeviewerlib.FullscreenActivity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+public static Activity activity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        activity=this;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public  void tellJoke(View view) {
+      //  Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+new EndpointsAsyncTask().execute( view.getContext() );
+    }
+
+    public static class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+        private static MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Context ... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder( AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        //
+                        .setRootUrl("http://192.168.1.122:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0];
+
+            try {
+                return myApiService.tellJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+            Intent mintent;
+
+            mintent = new Intent(context,FullscreenActivity.class);
+            mintent.putExtra( context.getString( R.string.JokeKey ), result );
+            context.startActivity( mintent );
+        }
+    }
+}
